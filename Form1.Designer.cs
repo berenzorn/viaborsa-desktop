3172803a26a95b8e7ca97c0b58614925cc072353
berenzorn <berenzorn@gmail.com>
2020-06-12 13:46:37 +0300

    ready 2 go

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace viaborsa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите прокси для подключения:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 60);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(104, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "fs.fabiobruno.ru";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(144, 60);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(110, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "fs1.fabiobruno.ru";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 111);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(115, 21);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(15, 141);
+            this.progressBar2.Maximum = 10000;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(285, 10);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 6;
+            this.progressBar2.Value = 10000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 161);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Viaborsa Desktop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace viaborsa {
+    public partial class Form1 : Form {
+        private static readonly String[] dns_names = new string[] { "<proxy 1>", "<proxy 2>" };
+        private static readonly int[] ports = new int[] { <port 1>, <port 2>, <port 3>, <port 4> };
+        private static readonly byte[] packetData = new byte[] { 0x00, 0x23, 0xf8, 0x4e, 0x45,
+            0x7e, 0xf4, 0x6d, 0x04, 0x39, 0xd2, 0xeb, 0x08, 0x00, 0x45, 0x00, 0x00, 0x34, 0x13,
+            0xea, 0x40, 0x00, 0x80, 0x06, 0x61, 0x19, 0xc0, 0xa8, 0x01, 0x22, 0x5d, 0x59, 0x66,
+            0x9d, 0x65, 0x48, 0x00, 0x50, 0x05, 0xe6, 0x36, 0xbb, 0x00, 0x00, 0x00, 0x00, 0x80,
+            0x02, 0x20, 0x00, 0x27, 0xde, 0x00, 0x00, 0x02, 0x04, 0x04, 0xec, 0x01, 0x03, 0x03,
+            0x08, 0x01, 0x01, 0x04, 0x02 };
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private String rdp_form(String addr) {
+            String[] rdp_array = new string[]
+            {
+                "authentication level:i:0", "negotiate security layer:i:1", "screen mode id:i:2",
+                "use multimon:i:0", "session bpp:i:16", "compression:i:1", "keyboardhook:i:2",
+                "audiocapturemode:i:0", "videoplaybackmode:i:1", "connection type:i:6",
+                "networkautodetect:i:0", "bandwidthautodetect:i:1", "displayconnectionbar:i:1",
+                "enableworkspacereconnect:i:0", "disable wallpaper:i:0", "allow font smoothing:i:1",
+                "allow desktop composition:i:1", "disable full window drag:i:0", "disable menu anims:i:0",
+                "disable themes:i:0", "disable cursor setting:i:0", "bitmapcachepersistenable:i:1",
+                "audiomode:i:2", "redirectprinters:i:1", "redirectcomports:i:0", "redirectsmartcards:i:0",
+                "redirectclipboard:i:1", "redirectposdevices:i:0", "autoreconnection enabled:i:1",
+                "prompt for credentials:i:0", "remoteapplicationmode:i:0", "gatewayusagemethod:i:4",
+                "gatewaycredentialssource:i:4", "gatewayprofileusagemethod:i:1", "promptcredentialonce:i:0",
+                "gatewaybrokeringtype:i:0", "use redirection server name:i:0", "rdgiskdcproxy:i:0"
+            };
+            String rdp_addr = "full address:s:" + addr;
+            int rnd = new Random().Next(135791113, 246810121);
+            String name = rnd.ToString() + ".rdp";
+            File.WriteAllLines(name, rdp_array);
+            using (StreamWriter file = new StreamWriter(name, true))
+                file.WriteLine(rdp_addr);
+            return name;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            IPAddress addr = null;
+            String proxy_name = "";
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            if (radioButton1.Checked) {
+                addr = Dns.GetHostAddresses(dns_names[0])[0];
+                proxy_name = dns_names[0];
+            }
+            else if (radioButton2.Checked) {
+                addr = Dns.GetHostAddresses(dns_names[1])[0];
+                proxy_name = dns_names[1];
+            }
+            progressBar1.Step = 5;
+            foreach (var port in ports) {
+                socket.SendTo(packetData, new IPEndPoint(addr, port));
+                for (int i = 0; i < 50; i++) {
+                    progressBar1.PerformStep();
+                    Application.DoEvents();
+                    Thread.Sleep(10);
+                }
+            }
+            Thread.Sleep(500);
+            Application.DoEvents();
+            String filename = rdp_form(proxy_name);
+            ProcessStartInfo rdp = new ProcessStartInfo();
+            rdp.FileName = "mstsc.exe";
+            rdp.Arguments = filename + " /f";
+            Process.Start(rdp);
+            progressBar2.Step = -5;
+            for (int i = 0; i < 2000; i++) {
+                Thread.Sleep(10);
+                progressBar2.PerformStep();
+                Application.DoEvents();
+            }
+            File.Delete(filename);
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
