18ff25a00c9065fbeb082818041f5d502291b0fc
berenzorn <berenzorn@gmail.com>
2020-08-21 12:33:09 +0300

    some fixes, error handlers

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,7 @@ namespace viaborsa {
             0x9d, 0x65, 0x48, 0x00, 0x50, 0x05, 0xe6, 0x36, 0xbb, 0x00, 0x00, 0x00, 0x00, 0x80,
             0x02, 0x20, 0x00, 0x27, 0xde, 0x00, 0x00, 0x02, 0x04, 0x04, 0xec, 0x01, 0x03, 0x03,
             0x08, 0x01, 0x01, 0x04, 0x02 };
+        private int counter = 500;
 
         public Form1() {
             InitializeComponent();
@@ -25,25 +26,54 @@ namespace viaborsa {
         private String rdp_form(String addr) {
             String[] rdp_array = new string[]
             {
-                "authentication level:i:0", "negotiate security layer:i:1", "screen mode id:i:2",
-                "use multimon:i:0", "session bpp:i:16", "compression:i:1", "keyboardhook:i:2",
-                "audiocapturemode:i:0", "videoplaybackmode:i:1", "connection type:i:6",
-                "networkautodetect:i:0", "bandwidthautodetect:i:1", "displayconnectionbar:i:1",
-                "enableworkspacereconnect:i:0", "disable wallpaper:i:0", "allow font smoothing:i:1",
-                "allow desktop composition:i:1", "disable full window drag:i:0", "disable menu anims:i:0",
-                "disable themes:i:0", "disable cursor setting:i:0", "bitmapcachepersistenable:i:1",
-                "audiomode:i:2", "redirectprinters:i:1", "redirectcomports:i:0", "redirectsmartcards:i:0",
-                "redirectclipboard:i:1", "redirectposdevices:i:0", "autoreconnection enabled:i:1",
-                "prompt for credentials:i:0", "remoteapplicationmode:i:0", "gatewayusagemethod:i:4",
-                "gatewaycredentialssource:i:4", "gatewayprofileusagemethod:i:1", "promptcredentialonce:i:0",
-                "gatewaybrokeringtype:i:0", "use redirection server name:i:0", "rdgiskdcproxy:i:0"
+                "allow desktop composition:i:0",
+                "allow font smoothing:i:0",
+                "audiocapturemode:i:0",
+                "audiomode:i:2",
+                "authentication level:i:0",         // 0!
+                "autoreconnection enabled:i:1",
+                "bandwidthautodetect:i:1",
+                "bitmapcachepersistenable:i:1",
+                "compression:i:1",
+                "connection type:i:7",
+                "disable full window drag:i:1",
+                "disable menu anims:i:1",
+                "disable themes:i:0",
+                "disable wallpaper:i:1",
+                "displayconnectionbar:i:1",
+                "domain:s:fs",                      // FS
+                "enableworkspacereconnect:i:1",
+                "keyboardhook:i:2",
+                "negotiate security layer:i:1",
+                "networkautodetect:i:1",
+                "prompt for credentials:i:0",
+                "promptcredentialonce:i:0",
+                "rdgiskdcproxy:i:0",
+                "redirectclipboard:i:1",
+                "redirectcomports:i:0",
+                "redirectposdevices:i:0",
+                "redirectprinters:i:1",
+                "redirectsmartcards:i:0",
+                "remoteapplicationmode:i:0",
+                "screen mode id:i:2",
+                "session bpp:i:16",
+                "use multimon:i:0",
+                "use redirection server name:i:0",
+                "videoplaybackmode:i:1"
             };
             String rdp_addr = "full address:s:" + addr;
             int rnd = new Random().Next(135791113, 246810121);
             String name = rnd.ToString() + ".rdp";
-            File.WriteAllLines(name, rdp_array);
+            try {
+                File.WriteAllLines(name, rdp_array);
+            }
+            catch (UnauthorizedAccessException) {
+                MessageBox.Show("Error. Can't create rdp-file.");
+                Application.Exit();
+            }
             using (StreamWriter file = new StreamWriter(name, true))
                 file.WriteLine(rdp_addr);
+            counter = 2000;
             return name;
         }
 
@@ -52,12 +82,24 @@ namespace viaborsa {
             String proxy_name = "";
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             if (radioButton1.Checked) {
-                addr = Dns.GetHostAddresses(dns_names[0])[0];
-                proxy_name = dns_names[0];
+                try {
+                    addr = Dns.GetHostAddresses(dns_names[0])[0];
+                    proxy_name = dns_names[0];
+                }
+                catch (SocketException) {
+                    MessageBox.Show("Error. Internet-connection don't working.");
+                    Application.Exit();
+                }
             }
             else if (radioButton2.Checked) {
-                addr = Dns.GetHostAddresses(dns_names[1])[0];
-                proxy_name = dns_names[1];
+                try {
+                    addr = Dns.GetHostAddresses(dns_names[1])[0];
+                    proxy_name = dns_names[1];
+                }
+                catch (SocketException) {
+                    MessageBox.Show("Error. Internet-connection don't working.");
+                    Application.Exit();
+                }
             }
             progressBar1.Step = 5;
             foreach (var port in ports) {
@@ -76,7 +118,7 @@ namespace viaborsa {
             rdp.Arguments = filename + " /f";
             Process.Start(rdp);
             progressBar2.Step = -5;
-            for (int i = 0; i < 2000; i++) {
+            for (int i = 0; i < counter; i++) {
                 Thread.Sleep(10);
                 progressBar2.PerformStep();
                 Application.DoEvents();
